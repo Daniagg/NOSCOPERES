36e73a8c8c1e30624ba53dcbaef8d466e4bbca25
Daniagg <daniagg1506@gmail.com>
2024-12-12 02:00:09 +0700

    Update INFMON.lua

--- INFMON.lua
+++ INFMON.lua
@@ -1,10 +1,1 @@
-        local Players = game:GetService("Players")
-        local LocalPlayer = Players.LocalPlayer
-        local stats = LocalPlayer:WaitForChild("stats")
-        local Money = stats:WaitForChild("Money")
 
-        local function updateMoney(newMoney)
-            Money.Value = newMoney
-        end
-
-        updateMoney(999999)
